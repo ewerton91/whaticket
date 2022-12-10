bfd603c2a3b5b7e38e38996c24d9d8789061c65e
ewerton91 <54516740+ewerton91@users.noreply.github.com>
2022-12-10 20:01:59 -0300

    Update pt.js

--- src/translate/languages/pt.js
+++ src/translate/languages/pt.js
@@ -25,7 +25,7 @@ const messages = {
         },
         buttons: {
           submit: "Entrar",
-          register: "Não tem um conta? Cadastre-se!",
+          register: "Não tem um conta ?? Cadastre-se!",
         },
       },
       auth: {
